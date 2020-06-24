288f9fe70c7416419b6e81e439bcdcc3b03be51e
Mike38 <mshar50055@gmail.com>
2020-06-24 23:59:05 +0300

    hamburger menu done

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+// Hamburger-menu
+var hamburgerButton = document.querySelector(".hamburger");
+var hamburgerMenu = document.querySelector(".nav");
+
+function closeHamburgerMenu() {
+  hamburgerMenu.classList.remove("active");
+  hamburgerButton.classList.remove("is-active");
+}
+
+function openHamburgerMenu() {
+  hamburgerMenu.classList.add("active");
+  hamburgerButton.classList.add("is-active");
+}
+
+hamburgerButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (hamburgerMenu.classList.contains("active")) {
+    closeHamburgerMenu();
+  } else {
+    openHamburgerMenu();
+  }
+});
+
+hamburgerMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains(".nav_link")) {
+    closeHamburgerMenu();
+  }
+});
+
+
 // Слайдер
 const left = document.querySelector("#left");
 const right = document.querySelector("#right");
